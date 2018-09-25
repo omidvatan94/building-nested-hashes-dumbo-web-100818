29e7c8b7e4b473a04a45adfcc1c5204cbd4504c2
Omid Vatandoost <omidvatan94@gmail.com>
2018-09-25 20:07:27 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -3,19 +3,16 @@ def third_challenge
    :montague => {
       :patriarch => {name: "Lord Montague", age: "53"
         },
-      :matriarch => {
-        #your key/value pairs here
+      :matriarch => {name: "Lady Montague", age: "54"
         },
-      :hero => {
-        #your key/value pairs here
+      :hero => {name: "Romeo", age: "15", status: "alive"
         },
       :hero_friends => []
    }, 
    :capulet => {
-      :patriarch => {
-        #your key/value pairs here
+      :patriarch => {name: "Lord Capulet", age: "50",
         },
-      :matriarch => {
+      :matriarch => {name: "Lady Capulet", age: 
         #your key/value pairs here
         },
       :heroine => {
