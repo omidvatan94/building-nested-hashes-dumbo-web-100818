5ff5d712f7cd3e4cbb31b2cb1a43769738f8e1a5
Omid Vatandoost <omidvatan94@gmail.com>
2018-09-25 20:14:22 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -19,7 +19,10 @@ def third_challenge
         },
       :heroine => {name: "Juliet", age: "15", status: "alive"
         },
-      :heroine_friends => []
+      :heroine_friends => [
+          {name: "Steven", age: "30", attitude: "confused"},
+          {name: "Nurse", age: "44", attitude: "worried"}
+      ]
    }
   }
 
