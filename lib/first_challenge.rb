e37b92a47808629959fa75fed683f7290a2ea772
Omid Vatandoost <omidvatan94@gmail.com>
2018-09-25 20:01:56 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,16 +1,2 @@
 def first_challenge
-   epic_tragedy = {
-     :montague => {
-       :patriarch => {},
-       :matriarch => {},
-       :hero => {},
-       :hero_friends => []
-     },
-     :capulet => {
-       :patriarch => {},
-       :matriarch => {},
-       :heroine => {},
-       :heroine_friends => []
-     }
-   }
-end
+   
