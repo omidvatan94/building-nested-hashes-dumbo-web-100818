e153cb8e329fcb2651ae24c0b902ddfcb3ffddc3
Omid Vatandoost <omidvatan94@gmail.com>
2018-09-25 20:08:17 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -12,8 +12,7 @@ def third_challenge
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50",
         },
-      :matriarch => {name: "Lady Capulet", age: 
-        #your key/value pairs here
+      :matriarch => {name: "Lady Capulet", age: "51",
         },
       :heroine => {
         #your key/value pairs here
